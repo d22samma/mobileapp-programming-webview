5b3303e3a32217674ffa0de266a66785bb3b406d
d22samma <d22samma@student.his.se>
2023-04-04 12:20:33 +0200

    Added External, Internal webpage with content

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -14,12 +14,14 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
     public void showExternalWebPage(){
-        // TODO: Add your code for showing external web page here
+        WebView myWebView = (WebView) findViewById(R.id.my_webview);
+        myWebView.loadUrl("https://www.his.se");
+    }
+    public void showInternalWebPage(){
+        WebView myWebView = (WebView) findViewById(R.id.my_webview);
+        myWebView.loadUrl("file:///android_asset/page.html");
     }
 
-    public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         myWebView = findViewById(R.id.my_webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
 
         /*
         * Rename your App. Tip: Values->Strings
@@ -73,11 +76,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
+            showExternalWebPage();
+
             Log.d("==>","Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
+            showInternalWebPage();
             Log.d("==>","Will display internal web page");
             return true;
         }
